fbe3265f7867a6e57f07efd6fbf08e646754857e
jti-jcamarena <jcamarena@journaltech.com>
2024-02-27 16:06:19 -0800

    updated

--- NIBRS_DISTRICT.groovy
+++ NIBRS_DISTRICT.groovy
@@ -1,7 +1,6 @@
 String caseCountryCode = _caseCounty != null ? _caseCounty : "";
-_judicialDistrict = DomainObject.find(LookupItem.class, "lookupList.name", "COUNTY", "code", caseCountryCode)?.find({it -> it != null})?.description;
-_judicialDistrict == null ? "" : _judicialDistrict;
-
+_judicialDistrict = DomainObject.find(LookupItem.class, "lookupList.name", "COUNTY", "code", caseCountryCode)?.find({it -> it != null})?.description ?: "MISSISSIPPI";
+_judicialDistrict == null || _judicialDistrict.isEmpty() ? "MISSISSIPPI" : _judicialDistrict;
 
 
 
---
+++ NIBRS_Map_Statute_UCR_Offense.groovy
@@ -0,0 +1,4 @@
+//$DomainObject.findUnique($metadataTool.entityClass("RuleDef"),"code","NIBRS_UCR")
+// .exec(null,{"statute": $screen.get('chargeNumber')}).getOutputValue("ucr")
+
+//_statute
